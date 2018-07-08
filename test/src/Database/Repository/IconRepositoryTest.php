57946e488d7957ee41c3ead5fe706a284e422c8b
BluePsyduck <bluepsyduck@gmx.com>
2018-07-08 18:36:36 +0200

    Added PHPUnit tests for some more repositories.

--- test/src/Database/Repository/IconRepositoryTest.php
+++ test/src/Database/Repository/IconRepositoryTest.php
@@ -24,6 +24,176 @@ use PHPUnit\Framework\TestCase;
 class IconRepositoryTest extends TestCase
 {
     /**
+     * Provides the data for testFindHashDataByTypesAndNames.
+     * @return array
+     */
+    public function provideFindHashDataByTypesAndNames(): array
+    {
+        return [
+            [[42, 1337], true],
+            [[], false]
+        ];
+    }
+
+    /**
+     * Tests the findHashDataByTypesAndNames method.
+     * @param array $modCombinationIds
+     * @param bool $expectWhere
+     * @covers ::findHashDataByTypesAndNames
+     * @dataProvider provideFindHashDataByTypesAndNames
+     */
+    public function testFindHashDataByTypesAndNames(array $modCombinationIds, bool $expectWhere)
+    {
+        $namesByTypes = ['foo' => ['abc', 'def'], 'bar' => ['ghi']];
+        $queryResult = [['jkl' => 'mno'], ['pqr' => 'stu']];
+
+        /* @var AbstractQuery|MockObject $query */
+        $query = $this->getMockBuilder(AbstractQuery::class)
+                      ->setMethods(['getResult'])
+                      ->disableOriginalConstructor()
+                      ->getMockForAbstractClass();
+        $query->expects($this->once())
+              ->method('getResult')
+              ->willReturn($queryResult);
+
+        /* @var QueryBuilder|MockObject $queryBuilder */
+        $queryBuilder = $this->getMockBuilder(QueryBuilder::class)
+                             ->setMethods(['select', 'innerJoin', 'andWhere', 'setParameter', 'getQuery'])
+                             ->disableOriginalConstructor()
+                             ->getMock();
+        $queryBuilder->expects($this->once())
+                     ->method('select')
+                     ->with([
+                         'IDENTITY(i.file) AS hash',
+                         'i.type AS type',
+                         'i.name AS name',
+                         'mc.order AS order'
+                     ])
+                     ->willReturnSelf();
+        $queryBuilder->expects($this->once())
+                     ->method('innerJoin')
+                     ->with('i.modCombination', 'mc')
+                     ->willReturnSelf();
+        $queryBuilder->expects($this->exactly($expectWhere ? 2 : 1))
+                     ->method('andWhere')
+                     ->withConsecutive(
+                         ['((i.type = :type0 AND i.name IN (:names0)) OR (i.type = :type1 AND i.name IN (:names1)))'],
+                         ['mc.id IN (:modCombinationIds)']
+                     )
+                     ->willReturnSelf();
+        $queryBuilder->expects($this->exactly($expectWhere ? 5 : 4))
+                     ->method('setParameter')
+                     ->withConsecutive(
+                         ['type0', 'foo'],
+                         ['names0', ['abc', 'def']],
+                         ['type1', 'bar'],
+                         ['names1', ['ghi']],
+                         ['modCombinationIds', $modCombinationIds]
+                     )
+                     ->willReturnSelf();
+        $queryBuilder->expects($this->once())
+                     ->method('getQuery')
+                     ->willReturn($query);
+
+        /* @var IconRepository|MockObject $repository */
+        $repository = $this->getMockBuilder(IconRepository::class)
+                           ->setMethods(['createQueryBuilder'])
+                           ->disableOriginalConstructor()
+                           ->getMock();
+        $repository->expects($this->once())
+                   ->method('createQueryBuilder')
+                   ->with('i')
+                   ->willReturn($queryBuilder);
+
+        $result = $repository->findHashDataByTypesAndNames($namesByTypes, $modCombinationIds);
+        $this->assertSame($queryResult, $result);
+    }
+
+    /**
+     * Provides the data for testFindIdDataByHashes.
+     * @return array
+     */
+    public function provideFindIdDataByHashes(): array
+    {
+        return [
+            [[42, 1337], true],
+            [[], false]
+        ];
+    }
+
+    /**
+     * Tests the findIdDataByHashes method.
+     * @param array $modCombinationIds
+     * @param bool $expectWhere
+     * @covers ::findIdDataByHashes
+     * @dataProvider provideFindIdDataByHashes
+     */
+    public function testFindIdDataByHashes(array $modCombinationIds, bool $expectWhere)
+    {
+        $hashes = ['12ab34cd', 'ab12cd34'];
+        $expectedHashes = [hex2bin('12ab34cd'), hex2bin('ab12cd34')];
+        $queryResult = [['jkl' => 'mno'], ['pqr' => 'stu']];
+
+        /* @var AbstractQuery|MockObject $query */
+        $query = $this->getMockBuilder(AbstractQuery::class)
+                      ->setMethods(['getResult'])
+                      ->disableOriginalConstructor()
+                      ->getMockForAbstractClass();
+        $query->expects($this->once())
+              ->method('getResult')
+              ->willReturn($queryResult);
+
+        /* @var QueryBuilder|MockObject $queryBuilder */
+        $queryBuilder = $this->getMockBuilder(QueryBuilder::class)
+                             ->setMethods(['select', 'innerJoin', 'andWhere', 'setParameter', 'getQuery'])
+                             ->disableOriginalConstructor()
+                             ->getMock();
+        $queryBuilder->expects($this->once())
+                     ->method('select')
+                     ->with([
+                         'i.id AS id',
+                         'i.type AS type',
+                         'i.name AS name',
+                         'mc.order AS order'
+                     ])
+                     ->willReturnSelf();
+        $queryBuilder->expects($this->once())
+                     ->method('innerJoin')
+                     ->with('i.modCombination', 'mc')
+                     ->willReturnSelf();
+        $queryBuilder->expects($this->exactly($expectWhere ? 2 : 1))
+                     ->method('andWhere')
+                     ->withConsecutive(
+                         ['i.file IN (:hashes)'],
+                         ['mc.id IN (:modCombinationIds)']
+                     )
+                     ->willReturnSelf();
+        $queryBuilder->expects($this->exactly($expectWhere ? 2 : 1))
+                     ->method('setParameter')
+                     ->withConsecutive(
+                         ['hashes', $expectedHashes],
+                         ['modCombinationIds', $modCombinationIds]
+                     )
+                     ->willReturnSelf();
+        $queryBuilder->expects($this->once())
+                     ->method('getQuery')
+                     ->willReturn($query);
+
+        /* @var IconRepository|MockObject $repository */
+        $repository = $this->getMockBuilder(IconRepository::class)
+                           ->setMethods(['createQueryBuilder'])
+                           ->disableOriginalConstructor()
+                           ->getMock();
+        $repository->expects($this->once())
+                   ->method('createQueryBuilder')
+                   ->with('i')
+                   ->willReturn($queryBuilder);
+
+        $result = $repository->findIdDataByHashes($hashes, $modCombinationIds);
+        $this->assertSame($queryResult, $result);
+    }
+
+    /**
      * Tests the findByIds method.
      * @covers ::findByIds
      */
@@ -60,13 +230,13 @@ class IconRepositoryTest extends TestCase
 
         /* @var IconRepository|MockObject $repository */
         $repository = $this->getMockBuilder(IconRepository::class)
-            ->setMethods(['createQueryBuilder'])
-            ->disableOriginalConstructor()
-            ->getMock();
+                           ->setMethods(['createQueryBuilder'])
+                           ->disableOriginalConstructor()
+                           ->getMock();
         $repository->expects($this->once())
-            ->method('createQueryBuilder')
-            ->with('i')
-            ->willReturn($queryBuilder);
+                   ->method('createQueryBuilder')
+                   ->with('i')
+                   ->willReturn($queryBuilder);
 
         $result = $repository->findByIds($ids);
         $this->assertSame($queryResult, $result);
