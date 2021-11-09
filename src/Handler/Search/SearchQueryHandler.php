d061292fe711849ff45af5435c20696fd52757e0
BluePsyduck <bluepsyduck@gmx.com>
2021-11-09 11:30:44 +0100

    Implemented tracking search requests.

--- src/Handler/Search/SearchQueryHandler.php
+++ src/Handler/Search/SearchQueryHandler.php
@@ -6,9 +6,12 @@ namespace FactorioItemBrowser\Api\Server\Handler\Search;
 
 use FactorioItemBrowser\Api\Client\Request\Search\SearchQueryRequest;
 use FactorioItemBrowser\Api\Client\Response\Search\SearchQueryResponse;
+use FactorioItemBrowser\Api\Database\Entity\Combination;
 use FactorioItemBrowser\Api\Search\SearchManagerInterface;
 use FactorioItemBrowser\Api\Server\Response\ClientResponse;
 use FactorioItemBrowser\Api\Server\Service\SearchDecoratorService;
+use FactorioItemBrowser\Api\Server\Service\TrackingService;
+use FactorioItemBrowser\Api\Server\Tracking\Event\SearchEvent;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
@@ -24,19 +27,26 @@ class SearchQueryHandler implements RequestHandlerInterface
 {
     private SearchDecoratorService $searchDecoratorService;
     private SearchManagerInterface $searchManager;
+    private TrackingService $trackingService;
 
     public function __construct(
         SearchDecoratorService $searchDecoratorService,
-        SearchManagerInterface $searchManager
+        SearchManagerInterface $searchManager,
+        TrackingService $trackingService,
     ) {
         $this->searchDecoratorService = $searchDecoratorService;
         $this->searchManager = $searchManager;
+        $this->trackingService = $trackingService;
     }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         /** @var SearchQueryRequest $clientRequest */
         $clientRequest = $request->getParsedBody();
+        /** @var Combination $combination */
+        $combination = $request->getAttribute(Combination::class);
+
+        $startTime = microtime(true);
 
         $searchQuery = $this->searchManager->parseQuery(
             Uuid::fromString($clientRequest->combinationId),
@@ -56,6 +66,17 @@ class SearchQueryHandler implements RequestHandlerInterface
         $response = new SearchQueryResponse();
         $response->results = $decoratedSearchResults; // @phpstan-ignore-line
         $response->totalNumberOfResults = $searchResults->count();
+
+        $trackingEvent = new SearchEvent();
+        $trackingEvent->combinationId = $clientRequest->combinationId;
+        $trackingEvent->modCount = $combination->getMods()->count();
+        $trackingEvent->locale = $clientRequest->locale;
+        $trackingEvent->queryString = $clientRequest->query;
+        $trackingEvent->resultCount = $response->totalNumberOfResults;
+        $trackingEvent->runtime = microtime(true) - $startTime;
+        $trackingEvent->cached = $searchResults->getIsCached();
+        $this->trackingService->addEvent($trackingEvent);
+
         return new ClientResponse($response);
     }
 }
---
+++ src/Tracking/Event/SearchEvent.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FactorioItemBrowser\Api\Server\Tracking\Event;
+
+use BluePsyduck\Ga4MeasurementProtocol\Attribute\Event;
+use BluePsyduck\Ga4MeasurementProtocol\Attribute\Parameter;
+use BluePsyduck\Ga4MeasurementProtocol\Request\Event\EventInterface;
+
+/**
+ * The event representing a search.
+ *
+ * @author BluePsyduck <bluepsyduck@gmx.com>
+ * @license http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+#[Event('search')]
+class SearchEvent implements EventInterface
+{
+    /**
+     * The locale used for the search.
+     * @var string|null
+     */
+    #[Parameter('locale')]
+    public ?string $locale = null;
+
+    /**
+     * The id of the combination used for the search.
+     * @var string|null
+     */
+    #[Parameter('combination_id')]
+    public ?string $combinationId = null;
+
+    /**
+     * The number of mods contained in the combination.
+     * @var int|null
+     */
+    #[Parameter('mod_count')]
+    public ?int $modCount = null;
+
+    /**
+     * The query string used for the search.
+     * @var string|null
+     */
+    #[Parameter('query_string')]
+    public ?string $queryString = null;
+
+    /**
+     * Whether the search results were already cached.
+     * @var bool|null
+     */
+    #[Parameter('cached')]
+    public ?bool $cached = null;
+
+    /**
+     * The number of results returned by the search.
+     * @var int|null
+     */
+    #[Parameter('result_count')]
+    public ?int $resultCount = null;
+
+    /**
+     * @var float|null The runtime of the search, in milliseconds.
+     */
+    #[Parameter('runtime')]
+    public ?float $runtime = null;
+}
